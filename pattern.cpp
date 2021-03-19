1a3a5e911952375b907cc726a3e12007458a3618
Vynidaulkel <kelvinnunezc@hotmail.com>
2021-03-18 22:16:53 -0600

    algorithm creation

--- pattern.cpp
+++ pattern.cpp
@@ -2,56 +2,49 @@
 #include"point.h"
 
 using namespace std;
-/*
-USAR lineas y escuadras
-Escribir resultados a un archivo
-Manipular tamannos con porcentajes
-
-area = ancho * altura.
-p = 2*b + 2+h.
-
-
--- forma --
-0-> dibujar vertical
-1-> dibujar horizontal
-*/
-string figuras [2] = {"linea","escuadra"};
 string colores [5] = {"pink","green","purple","yellow","red"}; //Colores utilizados
-int forma = 0;
-
-// El llenado del rectangulo sera siempre de lineas y escuadras
-// Se empezara siempre por la coordenada (0,0)
-// La anchura de la linea Sera de 3-4
-//
-
 void drawPattern(int pAncho, int pAltura){
-
-    unsigned int area = pAncho * pAltura;
+    int initX=0;
+    int initY=0;
+    int medX;
+    int medY;
+    int finalX=pAncho; 
+    int finalY=pAltura;    
     int color = 4;
-    Point punto = Point (0,0);
-
-    while(true){
-        string figuraActual = figuras[rand()%2];
-        string colorActual = colores[(++color)%5];
-
-        if(figuraActual == "linea"){
-            cout<<"linea-"<<colorActual<<"-";
-            switch(forma){
-            case 0:
-                cout<<"vertical-";
-                break;
-            case 1: 
-                cout<<"Horizontal-";
-                break;
-            }
-        }                                   
+    int ancho;
+    string colorActual;
+    
+    while(initX<=finalX && initY<=finalY){
+        colorActual = colores[(++color)%5];
+        ancho =  rand()%11+2;
+        cout << "cuadrado con puntos en: ("<< initX << ", " << initY 
+        << ") y en : ("<< finalX << ", " << finalY << ") y un ancho de linea de: " << ancho << " y el color es: " << colorActual<<  endl; 
+        initX+= ancho; 
+        initY+= ancho;
+        finalX-= ancho;
+        finalY-= ancho;
+    }
+    cout << "----------------------"<< endl;
+    int n = (rand()%7)+5;
+    for(int i=0; i< n; i++){
+        colorActual=  colores[rand()%5];
+        ancho =  rand()%11+2;
+        initX = rand()%pAncho; 
+        initY = rand()%pAltura;
+        finalX = rand()%pAncho;
+        finalY = rand()%pAltura;
+        medX = abs(finalX - initX);
+        medY = abs(finalY - initY);
+        cout << "escuadra con puntos en (" << initX << ", " << initY 
+        << "), en : ("<< medX << ", " << medY <<"), y en : ("<< finalX << ", " << finalY << ") y un ancho de linea de: " <<
+         ancho << " y el color es: " << colorActual<<  endl;
     }
 }
 
-
 int main(){
-
-
-
+    int tamagno;
+    cout << "digite el tamano de la pantalla, debe ser cuadrado" << endl;
+    cin >> tamagno;
+    drawPattern(tamagno, tamagno);
     return 0;
 }
